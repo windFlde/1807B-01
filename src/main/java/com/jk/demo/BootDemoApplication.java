0d193d73db1edbc14b9d81afe55ca161cffe4282
windFlde <wind13934954475@163.com>
2019-01-19 09:22:43 +0800

    本地

---
+++ src/main/java/com/jk/demo/BootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.jk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootDemoApplication.class, args);
+    }
+
+}
+
