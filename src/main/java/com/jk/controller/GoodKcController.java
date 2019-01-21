894baa5a77d7c365a6edfc55fcd4e84af3548fdd
root <1075118340@qq.com>
2019-01-21 16:24:27 +0800

    再次

--- src/main/java/com/jk/controller/GoodKcController.java
+++ src/main/java/com/jk/controller/GoodKcController.java
@@ -1,6 +1,6 @@
 package com.jk.controller;
 
-import com.jk.bean.Attr;
+import com.jk.bean.MallAttr;
 import com.jk.service.GoodKcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,7 +18,7 @@ public class GoodKcController {
 
     @ResponseBody
     @RequestMapping("getSx")
-    public List<Attr> getSx (String id){
+    public List<MallAttr> getSx (String id){
 
         return goodKcService.getSx(id);
     }
