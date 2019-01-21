5c96a248c1ffe4e864d68a39e938d4e3a5ad25be
zhenghang <1170723706@qq.com>
2019-01-21 13:09:29 +0800

    第三提交
    Signed-off-by: zhenghang <1170723706@qq.com>

---
+++ src/main/java/com/jk/controller/GoodsController.java
@@ -0,0 +1,21 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: boot_demo
+ * @description:
+ * @author: Mr.Xue
+ * @create: 2019-01-21 11:16
+ **/
+@Controller
+@RequestMapping("goods")
+public class GoodsController {
+
+    @RequestMapping("toView")
+    public String toView(String viewName){
+
+        return viewName;
+    }
+}
