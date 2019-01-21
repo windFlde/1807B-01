5118a05d3e0570ec458bdc0774233d76b61983d1
xjw163com <1632622685@qq.com>
2019-01-21 15:12:05 +0800

    薛嘉伟第一次提交

--- src/main/java/com/jk/controller/GoodsController.java
+++ src/main/java/com/jk/controller/GoodsController.java
@@ -2,6 +2,7 @@ package com.jk.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @program: boot_demo
