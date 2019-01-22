9c27a838702e6c2b20b8841cd1b18779c44452a1
xjw163com <1632622685@qq.com>
2019-01-22 11:26:56 +0800

    薛嘉伟提交的

--- src/main/java/com/jk/bean/MallAttr.java
+++ src/main/java/com/jk/bean/MallAttr.java
@@ -13,7 +13,7 @@ public class MallAttr {
     Integer id;
     String shxm_mch;
     String shfqy;
-    String flbh2;
+    Integer flbh2;
     @JsonFormat(pattern="yyyy-MM-dd",timezone="GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     Date chjshj;
