6d0fac67422660975d248baa291c8c4cedb89def
XZH <1170723706@qq.com>
2019-01-23 08:33:38 +0800

    第九次提交

--- src/main/java/com/jk/controller/GoodsController.java
+++ src/main/java/com/jk/controller/GoodsController.java
@@ -5,6 +5,7 @@ import com.jk.bean.MallAttr;
 import com.jk.bean.ReceivePage;
 import com.jk.bean.SendPage;
 import com.jk.service.GoodsService;
+import com.jk.utils.OssUpFileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -49,23 +50,23 @@ public class GoodsController {
     /**
      * 新增图片
      */
-//    String filename = "";
-//    @ResponseBody
-//    @PostMapping("toUploadBlog")
-//    public String toUploadBlog(@RequestParam("file") MultipartFile file){
-//        Map<String, Object> stringObjectMap = OssUpFileUtil.uploadFile(file);
-//        System.out.println(stringObjectMap);
-//        String count = "";
-//        for (String key : stringObjectMap.keySet()) {
-//            Object o = stringObjectMap.get(key);
-//            System.out.println("key: " + key + " value: " + o);
-//            if(key=="url"){
-//                count+=o;
-//                filename = count;
-//            }
-//        }
-//        return count;
-//    }
+    String filename = "";
+   @ResponseBody
+    @PostMapping("toUploadBlog")
+    public String toUploadBlog(@RequestParam("file") MultipartFile file){
+        Map<String, Object> stringObjectMap = OssUpFileUtil.uploadFile(file);
+       System.out.println(stringObjectMap);
+        String count = "";
+        for (String key : stringObjectMap.keySet()) {
+           Object o = stringObjectMap.get(key);
+            System.out.println("key: " + key + " value: " + o);
+           if(key=="url"){
+                count+=o;
+                filename = count;
+           }
+        }
+        return count;
+   }
     /*
        商品新增
     */
