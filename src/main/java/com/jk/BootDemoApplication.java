653a852b91a9cb2e1e71ae860c59b9efb2f55cfc
windFlde <wind13934954475@163.com>
2019-01-23 14:46:37 +0800

    两表新增

--- src/main/java/com/jk/BootDemoApplication.java
+++ src/main/java/com/jk/BootDemoApplication.java
@@ -4,12 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.jk.mapper")
-@EnableFeignClients
 public class BootDemoApplication {
 
     public static void main(String[] args) {
